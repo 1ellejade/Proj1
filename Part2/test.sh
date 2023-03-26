8b033585a42090c80d588ba2d5e9fed8d407a250
1ellejade <107277977+1ellejade@users.noreply.github.com>
2023-03-26 15:35:57 -0400

    Update test.sh

--- Part2/test.sh
+++ Part2/test.sh
@@ -6,8 +6,8 @@
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../shot_logs.csv /Part2/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/Part2/mapper1.py -mapper ../../mapreduce-test-python/Part2/mapper1.py \
--file ../../mapreduce-test-python/Part2/reducer1.py -reducer ../../mapreduce-test-python/Part2/reducer1.py \
+-file ../../Part2/mapper1.py -mapper ../../Part2/mapper1.py \
+-file ../../Part2/reducer1.py -reducer ../../Part2/reducer1.py \
 -input /Part2/input/* -output /Part2/output/
 
 /usr/local/hadoop/bin/hdfs dfs -cat /Part2/output/part-00000
