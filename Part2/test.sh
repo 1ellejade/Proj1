ff0abdfb7112d4fa0f0109444528d4a4c2512153
1ellejade <107277977+1ellejade@users.noreply.github.com>
2023-03-26 15:13:14 -0400

    Create test.sh

---
+++ Part2/test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+../../start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Part2/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Part2/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /Part2/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /Part2/input/
+
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file ../../mapreduce-test-python/Part2/mapper1.py -mapper ../../mapreduce-test-python/Part2/mapper1.py \
+-file ../../mapreduce-test-python/Part2/reducer1.py -reducer ../../mapreduce-test-python/Part2/reducer1.py \
+-input /Part2/input/* -output /Part2/output/
+
+/usr/local/hadoop/bin/hdfs dfs -cat /Part2/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Part2/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Part2/output/
+../../stop.sh
